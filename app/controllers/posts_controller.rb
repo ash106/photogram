13adc2ff1a78606344994be69d947a2b5b7840bf
Alex Howington <alex.howington@gmail.com>
2015-07-26 22:56:56 -0500

    Add flash messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,8 +15,10 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
+      flash[:success] = "Your post has been created!"
       redirect_to posts_url
     else
+      flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
       render :new
     end
   end
@@ -26,14 +28,17 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
-      redirect_to @post
+      flash[:success] = "Post updated."
+      redirect_to posts_url
     else
+      flash.now[:alert] = "Update failed.  Please check the form."
       render :edit
     end
   end
 
   def destroy
     @post.destroy
+    flash[:success] = "Post deleted."
     redirect_to posts_url
   end
 
