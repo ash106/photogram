bdae51eff85b5d97d695743fc9e32b21acaad3a9
Alex Howington <alex.howington@gmail.com>
2015-08-12 01:59:05 -0500

    Fix background images to cover entire body

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,17 @@ module ApplicationHelper
       image_tag 'placeholder.jpg', id: 'image-preview', class: 'img-responsive'
     end
   end
+
+  def current_location?(*args)
+    options = args.extract_options!
+    options.each do |key, val|
+        return false unless eval("controller.#{key.to_s}_name") == val
+    end
+    true
+  end
+
+  def background_image
+    return "login-bg" if current_location?(controller: "sessions", action: "new")
+    return "registration-bg" if current_location?(controller: "registrations", action: "new") || current_location?(controller: "registrations", action: "create")
+  end
 end
